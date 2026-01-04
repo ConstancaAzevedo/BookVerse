7d98a1a600e099aef17b60b3d458b42036c751ae
ConstancaAzevedo <constancaazevedo3@gmail.com>
2026-01-04 16:09:22 +0000

    versao H - quase final

--- src/services/sheetyConfig.js
+++ src/services/sheetyConfig.js
@@ -1,5 +1,5 @@
 export const SHEETY_CONFIG = {
-  PROJECT_ID: '3235b043c441642a9355a063e97eb68205a6709e6d5cfa379bf32bae971254f7',
+  PROJECT_ID: '441642a9355a063e97eb68205a6709e67',
   PROJECT_NAME: 'bookVerseBd',
   API_BASE: 'https://api.sheety.co/441642a9355a063e97eb68205a6709e6/bookVerseBd',
   API_KEY: 'ipt_iw_2526',
